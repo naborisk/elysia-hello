9cce3d650404308f40342a098ba25010f771f8b0
Naborisk <github@naborisk.com>
2023-09-10 22:11:48 +0900

    add todo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { Elysia } from 'elysia'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000)
+let todos: Array<string> = []
+
+const app = new Elysia()
+  .get('/todos', () => {
+    return todos
+  })
+  .post('/todos/add/:todo', ({ params }) => {
+    todos.push('todo')
+    return `added ${params.todo}`
+  })
+  .delete('/todos/delete/:todo', ({ params }) => {
+    todos = todos.filter(todo => todo !== params.todo)
+    return `deleted ${params.todo}`
+  })
+  .listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
